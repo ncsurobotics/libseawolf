68c5b2351f9f519ce96155bb5fa313a309813d2f
Chris Thunes <cmthunes@ncsu.edu>
2010-07-24 19:57:32 -0400

    Blackfin can now build the hub

--- build/config.bfin.mk
+++ build/config.bfin.mk
@@ -1,14 +1,8 @@
 
-# Build options for crosscompiling libseawolf under Linux. This does not build
-# the hub by default, but if you have sqlite3 in place for a Blackfin
-# environment feel free to remove the line below that disable the building of
-# the hub.
+# Build options for crosscompiling libseawolf under Linux.
 
 # Build options
 CC = bfin-linux-uclibc-gcc
 
 # Install prefix
 PREFIX ?= bfin
-
-# Remove the hub from the build process
-HUB_NAME =
