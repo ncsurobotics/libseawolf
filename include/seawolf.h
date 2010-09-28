947bf037689c2888e02a3288923da9bfc1fab728
Chris Thunes <cmthunes@ncsu.edu>
2010-09-28 21:40:10 +0200

    Cleanup seawolf.h and move some documentation that should be part of the main page

--- include/seawolf.h
+++ include/seawolf.h
@@ -3,15 +3,6 @@
  * \brief Top level libseawolf include
  */
 
-
-/**
- * \defgroup Core Core Routines
- * \defgroup Communications Communications
- * \defgroup DataStructures Data Structures
- * \defgroup Hardware Hardware Access
- * \defgroup Utilities Utilities
- */
-
 #ifndef __SEAWOLF_ROOT_INCLUDE_H
 #define __SEAWOLF_ROOT_INCLUDE_H
 
@@ -25,34 +16,31 @@
  */
 #define _XOPEN_SOURCE 600
 
-/* Make sure some standard includes are available */
+/* Make some standard includes available */
 #include <assert.h>
 #include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <stdint.h>
 
 /* Include all Seawolf development headers */
-#include "seawolf/comm.h"
-#include "seawolf/logging.h"
-#include "seawolf/var.h"
-#include "seawolf/notify.h"
-
-#include "seawolf/serial.h"
 #include "seawolf/ardcomm.h"
-
+#include "seawolf/comm.h"
 #include "seawolf/config.h"
-#include "seawolf/util.h"
-#include "seawolf/timer.h"
-#include "seawolf/task.h"
-#include "seawolf/pid.h"
-
-#include "seawolf/stack.h"
-#include "seawolf/list.h"
-#include "seawolf/queue.h"
 #include "seawolf/dictionary.h"
+#include "seawolf/list.h"
+#include "seawolf/logging.h"
+#include "seawolf/notify.h"
+#include "seawolf/pid.h"
+#include "seawolf/queue.h"
+#include "seawolf/serial.h"
+#include "seawolf/stack.h"
+#include "seawolf/task.h"
+#include "seawolf/timer.h"
+#include "seawolf/util.h"
+#include "seawolf/var.h"
 
 /**
  * \addtogroup Main
