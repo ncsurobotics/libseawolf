d98cf69001b8ef578bf39e2a2c38f5cdce2552ee
Christopher Thunes <cmthunes@ncsu.edu>
2011-06-28 20:24:11 -0400

    Add subscription calls to the Python module

--- src/python/var.py
+++ src/python/var.py
@@ -4,3 +4,9 @@ from . import seawolf as _sw
 get = _sw.Var_get
 set = _sw.Var_set
 setAutoNotify = _sw.Var_setAutoNotify
+
+stale = _sw.Var_stale
+poked = _sw.Var_poked
+subscribe = _sw.Var_subscribe
+unsubscribe = _sw.Var_unsubscribe
+sync = _sw.Var_sync
